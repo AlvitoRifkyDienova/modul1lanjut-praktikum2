ed3bed5f60647f8987f4e73c75d5a372695caeb3
unknown <alvito_rifky_29rpl@student.smktelkom-mlg.sch.id>
2021-01-15 00:14:41 +0700

    Initial Commit

---
+++ src/modul/pkg1/lanjut/praktikum2/paraktikum2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modul.pkg1.lanjut.praktikum2;
+
+/**
+ *
+ * @author MOKLET-1
+ */
+public class paraktikum2 {
+     public static void main(String[]args){
+       int i,k,j;
+       for (i=1; i<=4; i++) {
+       for (j=1; j<=i; j++) {
+       System.out.print("@"); 
+   }
+       System.out.println(); 
+  }
+}
+}
